30bfd9196a5833462c20a1239973854a953f4086
malangadan <malangadan@ufl.edu>
2024-04-05 00:14:48 -0400

    Unit tests completed full coverage

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
+
+let component;
 
 beforeEach(() => {
-  // Render the Counter component here
-})
+  component = render(<Counter />);
+});
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = component.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countElement = component.getByTestId('count');
+  expect(countElement.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const addButton = component.getByText('+');
+  const countElement = component.getByTestId('count');
+
+  fireEvent.click(addButton);
+  expect(countElement.textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const minusButton = component.getByText('-');
+  const countElement = component.getByTestId('count');
+
+  fireEvent.click(minusButton);
+  expect(countElement.textContent).toBe('-1');
 });
